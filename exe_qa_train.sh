083a7eeb75badd9660ca8d986586caf5e12d8a88
Tingxuan Li <tingxuanli0829@craft.nlp>
2022-05-25 00:01:31 +0900

    First commit

---
+++ exe_qa_train.sh
@@ -0,0 +1,14 @@
+CUDA_VISIBLE_DEVICES=1 python run_qa_mod.py \
+  --model_name_or_path bert-base-multilingual-uncased \
+  --train_file ./dataset/train_factoid/train_v2.4_mod_neo.json\
+  --test_file ./dataset/test_factoid/test_v2.4_neo.json\
+  --do_train \
+  --do_eval False\
+  --do_predict False\
+  --per_device_train_batch_size 12 \
+  --learning_rate 3e-5 \
+  --num_train_epochs 2 \
+  --max_seq_length 384 \
+  --doc_stride 128 \
+  --version_2_with_negative True \
+  --output_dir ./mbert_trained_factoid_2.0/
